bb3e8aa8667e293eeb2d894b659dc6b627793982
BAILLY Michael <mickael.bailly@free.fr>
2022-11-23 14:39:56 +0100

    Add pagination to the trades table

--- src/lib/widgets/TradesTable.svelte
+++ src/lib/widgets/TradesTable.svelte
@@ -2,11 +2,23 @@
 	import type { DashboardTrade } from '$lib/types/DashboardTrade';
 	import { getFamilyLabel } from '$lib/volumeReference';
 	import Pnl from '$lib/widgets/Pnl.svelte';
-	import DataTable, { Body, Cell, Head, Label, Row } from '@smui/data-table';
+	import DataTable, { Body, Cell, Head, Label, Pagination, Row } from '@smui/data-table';
+	import IconButton from '@smui/icon-button';
 	import { format, formatDistance } from 'date-fns';
 	import MoreLink from './MoreLink.svelte';
 
 	export let trades: DashboardTrade[];
+
+	const rowsPerPage = 100;
+
+	let showPagination = false;
+	let page = 0;
+	let rows: DashboardTrade[] = [];
+
+	$: {
+		rows = trades.slice(page * rowsPerPage, (page + 1) * rowsPerPage);
+		showPagination = trades.length > rowsPerPage;
+	}
 </script>
 
 <DataTable style="width: 100%;">
@@ -70,4 +82,37 @@
 			</Row>
 		{/each}
 	</Body>
+	{#if showPagination}
+		{@const lastPageIndex = Math.ceil(trades.length / rowsPerPage) - 1}
+		<Pagination slot="paginate">
+			<svelte:fragment slot="total">
+				{page * rowsPerPage + 1}-{(page + 1) * rowsPerPage} of {trades.length}
+			</svelte:fragment>
+
+			<IconButton
+				class="material-icons"
+				action="first-page"
+				title="First page"
+				on:click={() => (page = 0)}
+				disabled={page === 0}>first_page</IconButton>
+			<IconButton
+				class="material-icons"
+				action="prev-page"
+				title="Prev page"
+				on:click={() => page--}
+				disabled={page === 0}>chevron_left</IconButton>
+			<IconButton
+				class="material-icons"
+				action="next-page"
+				title="Next page"
+				on:click={() => page++}
+				disabled={page === lastPageIndex}>chevron_right</IconButton>
+			<IconButton
+				class="material-icons"
+				action="last-page"
+				title="Last page"
+				on:click={() => (page = lastPageIndex)}
+				disabled={page === lastPageIndex}>last_page</IconButton>
+		</Pagination>
+	{/if}
 </DataTable>
