98ce7f930a40f502c4c4d1485136cb2cfc77f565
Michael Bailly <mickael.bailly@free.fr>
2022-12-29 15:45:01 +0100

    Use aggregation framework for all /history/v page

--- src/lib/server/db/history-volume.ts
+++ src/lib/server/db/history-volume.ts
@@ -1,7 +1,65 @@
+import { FEE_BUY, FEE_SELL } from '$lib/tradeUtils';
 import type { TradeTimeRangeMongoQuery } from '$lib/types/TradeTimeRangeMongoQuery';
 import type { TradeTimeRangeOpts } from '$lib/types/TradeTimeRangeOpts';
 import type { Watcher } from '$lib/types/Watcher';
 import { getTradeCollection } from '.';
+
+type PnlPerTypePerVolumeFamilyMongoQuery = {
+	volumeFamily: {
+		$exists: boolean;
+	};
+} & TradeTimeRangeMongoQuery;
+
+export async function getPnlPerTypePerVolumeFamily(opts: TradeTimeRangeOpts) {
+	const query: PnlPerTypePerVolumeFamilyMongoQuery = {
+		volumeFamily: { $exists: true }
+	};
+
+	if (opts.start && opts.start instanceof Date) {
+		query.$and = [{ boughtTimestamp: { $gte: opts.start } }];
+	}
+	if (opts.end && opts.end instanceof Date) {
+		if (!query.$and) {
+			query.$and = [];
+		}
+		query.$and.push({ boughtTimestamp: { $lt: opts.end } });
+	}
+
+	const pipeline = [];
+	pipeline.push({ $match: query });
+	pipeline.push({
+		$addFields: {
+			netPnl: {
+				$subtract: [
+					{ $subtract: [{ $multiply: ['$soldAmount', '$soldPrice'] }, '$quoteAmount'] },
+					{
+						$add: [
+							{ $multiply: ['$quoteAmount', FEE_BUY] },
+							{ $multiply: ['$soldAmount', '$soldPrice', FEE_SELL] }
+						]
+					}
+				]
+			}
+		}
+	});
+
+	pipeline.push({
+		$group: {
+			_id: {
+				$concat: ['$volumeFamily', ' ', '$watcher.type', ' ', '$watcher.config']
+			},
+			watcher: { $first: '$watcher' },
+			volumeFamily: { $first: '$volumeFamily' },
+			tradeCount: { $sum: 1 },
+			pnl: { $sum: '$netPnl' }
+		}
+	});
+
+	const collection = await getTradeCollection();
+	const pipelineResponse = await collection.aggregate(pipeline).toArray();
+
+	return pipelineResponse;
+}
 
 export async function getGroupsPerType(opts: TradeTimeRangeOpts) {
 	const query: TradeTimeRangeMongoQuery = {};
--- src/routes/history/v/+page.server.ts
+++ src/routes/history/v/+page.server.ts
@@ -1,7 +1,4 @@
 import { getAtMidnightUTC, parseMonthStringOrNow } from '$lib/dates';
-import { getTrades } from '$lib/server/db/trades';
-import { getNetPnl } from '$lib/tradeUtils';
-import type { DashboardTrade } from '$lib/types/DashboardTrade';
 import { add, format } from 'date-fns';
 import { computeBestGroupPerType, computePerTypePerVolumeFamily } from './helper';
 
@@ -11,16 +8,10 @@ export async function load({ url }: { url: URL }) {
 	const [year, month] = parseMonthStringOrNow(period);
 	const start = getAtMidnightUTC(year, month, 1);
 	const end = add(start, { months: 1 });
-	const trades = await getTrades({ start, end });
 	const realPeriod = format(add(start, { days: 10 }), 'yyyy-MM');
 
-	const dashboardTrades: DashboardTrade[] = trades.map((trade) => ({
-		...trade,
-		netPnl: getNetPnl(trade)
-	}));
-
 	const bestGroupPerType = computeBestGroupPerType({ start, end });
-	const pnlPerType = computePerTypePerVolumeFamily(dashboardTrades);
+	const pnlPerType = computePerTypePerVolumeFamily({ start, end });
 
 	return {
 		pnlPerType,
